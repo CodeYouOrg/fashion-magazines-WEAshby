7566ffc9ce7e64868be7ad4ef89d82b8e7c20843
william-ashby <weashby01@gmail.com>
2024-10-17 20:06:22 -0400

    first push

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,15 @@
 -- Add your sql here
+SELECT 
+    c.customer_name AS Customer,
+    PRINTF('$%.2f', SUM(s.price_per_month * s.subscription_length)) AS "Amount Due"
+-- orders alias to o    
+FROM 
+    orders o
+-- join customer table, alias to c    
+JOIN 
+    customers c ON o.customer_id = c.customer_id
+-- join subscriptions table, alias to s    
+JOIN 
+    subscriptions s ON o.subscription_id = s.subscription_id
+
+
